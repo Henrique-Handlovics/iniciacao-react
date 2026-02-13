c764a334a9c701fdfffd2068692fb121f2455009
Henrique Handlovics <henriquehandlovicssousa@gmail.com>
2026-02-13 07:55:06 -0300

    icon novo

--- src/componets/Tesks.jsx
+++ src/componets/Tesks.jsx
@@ -1,4 +1,4 @@
-import { ChevronRightIcon, TrashIcon } from 'lucide-react';
+import { ChevronRightIcon, TrashIcon, CheckIcon } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
 function Tasks({ tasks, onTaskClick, onTaskDelete }){
@@ -16,8 +16,9 @@ function Tasks({ tasks, onTaskClick, onTaskDelete }){
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
         {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-            <button onClick={() => onTaskClick(task.id)} className={`w-full text-left bg-slate-400 text-white p-2 rounded-md 
+            <button onClick={() => onTaskClick(task.id)} className={`flex items-center gap-2 w-full text-left bg-slate-400 text-white p-2 rounded-md 
                 ${task.isCompleted && 'line-through'}`}>
+                {task.isCompleted && <CheckIcon />}
                 {task.title}   
             </button>
             <button onClick={() => onSeeDetailsClick(task)} className='bg-slate-400 p-2 rounded-md text-white'>
